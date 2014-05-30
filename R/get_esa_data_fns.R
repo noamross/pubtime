49018deca5b6ca3a599101ed618d69271d352cd7
Noam Ross <noam.ross@gmail.com>
2014-05-29 23:40:56 -0700

    Working fns for pulling data from ESA TOCs + abstracts

--- R/get_esa_data_fns.R
+++ R/get_esa_data_fns.R
@@ -1,27 +1,30 @@
 # Functions to extract article data from the ESA website
 
+#' Pull all the URLs of abstracts from an ESA journal table of contents
 #' @export
 #' @import httr stringr
-get_esa_abtract_urls <- function(journal, vol, issue) {
-  URL = paste0("http://www.esajournals.org/toc/", journal, "/", vol, "/" issue)
-  toc_page = GET(URL)
-  abstracts = str_extract_all(toc_page, 
-  #TODO: write regex for this: "<a class="ref nowrap " href="/doi/abs/10.1890/12-1339.1">Abstract</a>"
-  return(abstracts)
+get_esa_abtract_urls = function(journal, vol, issue) {
+  URL = paste0("http://www.esajournals.org/toc/", journal, "/", vol, "/", issue)
+  toc_page =getURL(URL, cookiejar=tempfile(), followlocation=TRUE)
+  ab_urls = str_match_all(toc_page,
+                          '(/doi/abs/10\\.[0-9]{4,}/[0-9\\.-]+)">Abstract</a>'
+                          )[[1]][,2]
+  ab_urls = paste0("http://www.esajournals.org", ab_urls)
+  return(ab_urls)
 }
 
+#' Get some (limited) data on an ESA journal article
 #' @export
 #' @import httr stringr
-get_esa_article_data <- function(url) {
-
-  journal
-  vol
-  issue
-  doi
-  doidate
-  publishdate
-  editor
-  
-  datefields
-  return(
+get_esa_article_data = function(url) {
+  ab_page = getURL(url, cookiejar=tempfile(), followlocation=TRUE)
+  out = list(doi = str_match(ab_page, '<meta name="dc.Identifier" scheme="doi" content="(10\\.[0-9]{4,}/[0-9\\.-]+)" />')[,2],
+             vol = str_match(ab_page, 'Volume ([0-9]{1,2}),')[,2],
+             issue = str_match(ab_page, 'Issue ([0-9]{1,2})')[,2],
+             publishdate = str_match(ab_page, '<meta name="dc.Date" scheme="WTN8601" content="([0-9-]+)" />')[,2],
+             editor = str_match(ab_page, 'Editor: (.*)\\.</p>')[,2])  #consider using md5::md5(raw("editor")) to anonymize
+  dates = str_match_all(ab_page, '(Received|Revised|Accepted|Final version received): ([[:alnum:][:space:],]+)[;<]')
+  datefields = as.list(dates[[1]][,3])
+  names(datefields) = dates[[1]][,2]
+  return(c(out, datefields))
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-A project to examine journal acceptance times
+# A project to examine journal acceptance times
+
+This is organized as an R package. While it can be installed with
+`devtools::install_github("noamross/journaltimes")`, I recommend
+cloning the 
