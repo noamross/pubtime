044f1b9f656991ca7f41956b1289d957e7f3becc
Noam Ross <noam.ross@gmail.com>
2014-05-30 10:39:40 -0700

    Fixed get_esa_abstract_urls to work with Ecosphere

--- R/get_esa_data_fns.R
+++ R/get_esa_data_fns.R
@@ -7,7 +7,7 @@ get_esa_abtract_urls = function(journal, vol, issue) {
   URL = paste0("http://www.esajournals.org/toc/", journal, "/", vol, "/", issue)
   toc_page =getURL(URL, cookiejar=tempfile(), followlocation=TRUE)
   ab_urls = stri_match_all_regex(toc_page,
-                          '(/doi/abs/10\\.[0-9]{4,}/[0-9\\.-]+)">Abstract</a>'
+                          '(/doi/abs/10\\.[0-9]{4,}/[[:alnum:]]\\.-]+)">Abstract</a>'
                           )[[1]][,2]
   ab_urls = paste0("http://www.esajournals.org", ab_urls)
   return(ab_urls)
