1d45639550bd24e353fb252cec6e49c54a43b681
Noam Ross <noam.ross@gmail.com>
2014-06-18 08:31:14 -0700

    get_dois function added.  Pulls by ISSN and year.

---
+++ R/get_dois.R
@@ -0,0 +1,14 @@
+#' Use crossref metadata search to get all DOIs for a journal over a time period
+#' @param ISSN The journal ISSN number
+#' @param years a vector of years for which
+#' @export
+#' @import rcrossref plyr stringi
+get_dois = function(ISSN, years) {
+  dois = adply(years, 1, function(i) {
+                d = cr_search(query=ISSN, sort="year", type="Journal Article",
+                    rows=1000, year=i)
+                if(class(d) == "character") return(NULL) else return(d) 
+          })
+  return(stri_replace_first_fixed(dois$doi, "http://dx.doi.org/", ""))
+}
+
