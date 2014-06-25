e708cf960a40b3e7f57cca05f0da128a0fa9b706
Noam Ross <noam.ross@gmail.com>
2014-06-24 17:06:43 -0700

    Script to collect and aggregate data

---
+++ inst/pubtime_data.R
@@ -0,0 +1,26 @@
+#load and concatenate all of the journal data files.
+data_locs = c(file.path("..", "inst", "journal_data"),
+              file.path("..", "journal_data"),
+              file.path("journal_data"),
+              file.path("inst", "journal_data"))
+
+
+data_loc = data_locs[file.exists(data_locs)]
+
+
+pubtime_files = file.path(data_loc, 
+                          list.files(data_loc, pattern=".*_pubtimes\\.csv"))
+
+
+pubtime_list = lapply(pubtime_files, utils::read.csv)
+
+
+if(length(pubtime_list) > 1) {
+  pubtimes = do.call(pubtime_list, rbind)
+} else {
+  pubtimes = pubtime_list[[1]]
+}
+
+rm(data_locs, data_loc, pubtime_files, pubtime_list)
+pubtimes[,5:13] = lapply(pubtimes[,5:13], as.Date)
+save(pubtimes, file="data/pubtimes.rda")
