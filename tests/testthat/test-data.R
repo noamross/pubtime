607b10a6068ce9242baad4283e5e55ea14e662d5
Noam Ross <noam.ross@gmail.com>
2014-06-25 17:01:11 -0700

    Created tests to check quality of data files.

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -14,11 +14,35 @@ test_that("get_pub_history works", {
   expect_identical(testval, testpub)
 })
   
-#
-#context("data")
-#
-#test_that("Provided data has correct columns", {
-#})
-#
-#test_that("All dates are in correct format" {
-#})
+context("data")
+
+#for these tests, load the csv files from journal_data
+data_path = file.path(system.file("journal_data", package="pubtime"))
+pubtimes_files = list.files(data_path, pattern=".*_pubtimes\\.csv")
+pubtimes_list = lapply(file.path(data_path, pubtimes_files), utils::read.csv)
+names(pubtimes_list) = pubtimes_files
+
+for(filename in names(pubtimes_list)) {
+
+  test_that(paste(filename, "has correct headers"), {
+    
+    expect_that(names(pubtimes_list[[filename]]), is_identical_to(
+      c("doi", "journal", "volume", "editor", "submitted", "revised",
+        "decision", "accepted", "preprint", "finalversion", "online",
+        "issueonline", "issuedate")))
+  })
+  
+  test_that(paste(filename, "has no empty rows"), {
+    expect_that(all(apply(pubtimes_list[[filename]][4:13],1,
+                          function(x)any(!is.na(x)))), is_true())
+  })
+  
+  test_that(paste(filename, "has correctly formatted editors"), {
+    expect_that(!any(stri_detect_regex(pubtimes_list[[filename]]$editor, 
+                                       "['\"`]")), is_true())
+    expect_that(!any(stri_detect_regex(pubtimes_list[[filename]]$editor,
+                                       "\\s(?!II)[[:upper:]]{2}\\s")), is_true())
+  })
+                                                      
+  
+}
