3e65134a170d08fa596ec6ef4329fa41fc8fc343
Noam Ross <noam.ross@gmail.com>
2014-06-26 21:34:47 -0700

    First commit of new yaml-based design

--- R/get_pub_history.R
+++ R/get_pub_history.R
@@ -1,63 +1,95 @@
-#' Retreive the publication history of a journal article from its DOI
-#' 
-#' @param doi The DOI of the journal article
-#' @param oa_only When TRUE, the function will not attempt to retreat information
-#' from articles whose publication histories are behind a paywall
-#' @return A list with the DOI, journal, volume, editor and publication history
-#' dates of the article. Unavailable publication history dates will be NA. The
-#' possible categories of dates are:
-#' 
-#' \item{submitted}{The date the article was sent in}
-#' \item{decision}{The date an initial decision (e.g., major revisions), was reached on the article}
-#' \item{revised}{The date an updated version of the article was sent}
-#' \item{accepted}{The date the article was accepted for publication}
-#' \item{finalversion}{The date a finalized version was received, following an initial or final acceptance decision}
-#' \item{preprint}{The date a pre-typeset version of the article appeared online}
-#' \item{online}{The date the article appeared online}
-#' \item{issueonline}{The date the issue that the article is in appeared online}
-#' \item{issuedate}{The date the issue was published}
-#' 
-#' Most articles do not have all this information.  In general, one expects to
-#' have dates for submission, acceptance, and publication
-#' @export
-#' @import rcrossref XML
-get_pub_history = function(doi, oa_only=FALSE) {
+scraper_loc = system.file("scrapers", package="pubtime")
+scraper_files = file.path(scraper_loc, list.files(scraper_loc,
+                                                 pattern="[^.]\\.yaml"))
+scrapers = plyr::alply(scraper_files, 1, yaml::yaml.load_file)
+
+
+#' @importFrom rcrossref cr_cn
+#' @importFrom plyr ldply adply
+#' @export 
+get_pub_history = function(doi, verbose=TRUE) {
+  #check if doi is in local repo
+  #check if doi is in main dataset
+  #get info from crossref
+  if(verbose) message("Getting crossref metadata")
   citation_xml = cr_cn(doi, "crossref-xml")
+  if (!is.list(citation_xml)) citation_xml = list(citation_xml)
   
-  pubhistory=list()
-  pubhistory$doi = doi
-  pubhistory$journal = xpathSApply(citation_xml, "//full_title", xmlValue)
-  pubhistory$volume = xpathSApply(citation_xml, "//journal_volume/volume",
-                                  xmlValue) 
-  
-  jmatch = journals$name %in% pubhistory$journal
-  
-  if(!any(jmatch)) {
-    stop(paste("Scraping for", pubhistory$journal, "is not (yet) supported"))
-  } else if(!journals$open[jmatch] & oa_only) {
-    stop(paste("Publication dates for", pubhistory$journal,
-         "not available without subscription access"))
-  } else {
-    pubhistory = c(pubhistory, 
-                   do.call(journals$scraper[jmatch], list(doi=pubhistory$doi)))
-  }
-  
-  pubhistory = standardize_datenames(pubhistory)
-  return(pubhistory)
+  pubhistory_df = ldply(citation_xml, function(z) {
+                    data.frame(doi = xpathSApply(z, "//journal_article/doi_data/doi", xmlValue),
+                               journal = xpathSApply(z, "//journal/journal_metadata/full_title", xmlValue),
+                               url = xpathSApply(z, "//journal_article/doi_data/resource", xmlValue))
+                        })
+
+  if(verbose) message("Scraping...")
+  pubhistory_df = adply(pubhistory_df, 1, scrape, 
+                        .progress=ifelse(verbose, 'time', 'none'))
+  return(pubhistory_df)
 }
 
-#' Convert the terms for publications provided by the publishers to standard
-#' names
-standardize_datenames = function(pubhistory) {
-  names(pubhistory) = tolower(names(pubhistory))
-  for(i in 1:length(datenames)) {
-    matches = names(pubhistory) %in% datenames[[i]]
-    if(!any(matches)) {
-      pubhistory[[names(datenames)[i]]] = NA
-    } else {
-      names(pubhistory)[matches] = names(datenames)[i]
-    }
+#' @importFrom plyr laply
+scrape = function(pubhistory) {
+  scraper_no = which(laply(scrapers, function(z) {
+                             pubhistory$journal %in% z$journals}))
+  if(length(scraper_no) == 0) {
+    scraper_no = which(laply(scrapers, function(z) {
+                               stri_detect_regex(pubhistory$url, z$url) }))
   }
-  pubhistory = pubhistory[c("doi", "journal", "volume", 
-                            names(datenames))]
+  
+  if(length(scraper_no) == 0) stop("Journal not supported")
+  
+  scraper = scrapers[[scraper_no]]
+  
+  article_XML = htmlTreeParse(content(slowGET(pubhistory$url), "text"),
+                              useInternalNodes=T, asText=TRUE)
+  if(!is.null(scraper$full_url)) {
+    full_url_node = getNodeSet(article_XML, scraper$full_url$selector)[[1]]
+    full_url_val = xmlGetAttr(full_url_node, scraper$full_url$attribute)
+    full_XML = htmlTreeParse(content(slowGET(full_url_val), "text"),
+                                useInternalNodes=T, asText=TRUE)
+  }
+  
+  vals = llply(scraper$elements, function(element) {
+   if(is.null(element$full_url)) {
+     page_XML = article_XML
+   } else {
+     page_XML = full_XML
+   }
+    node = getNodeSet(page_XML, element$selector)
+    if(length(node) == 0) {
+      return(NA)
+    }
+    if(is.null(element$attribute)) {
+      text = laply(node, xmlValue)
+    } else {
+      text = laply(node, xmlGetAttr, element$attribute)
+      names(text) = NULL
+    }
+    if(is.null(element$regex)) {
+      match = text
+    } else {
+      match = unlist(stri_match_all_regex(text, element$regex,
+                           stri_opts_regex(case_insensitive=TRUE)))
+      match = match[!is.na(match)]
+    }
+    if(is.null(element$date_format)) {
+      val = match 
+    } else {
+      for(dateform in element$date_format) {
+        if(!stri_detect_fixed(dateform, "%d")) {
+          dateform = paste0("%d-", dateform)
+          match = paste0("01-", match)
+        }
+        val = as.character(as.Date(match, format=dateform))
+        if(class(val) != "try-error") {
+          break
+        }
+      }
+    }
+    
+    val = val[order(val)]
+    
+    return(val)
+  })
+  return(cbind(pubhistory, t(as.data.frame(unlist(vals)))))
 }
---
+++ R/slowGET.R
@@ -0,0 +1,40 @@
+throttle = new.env(parent = emptyenv())
+throttle$recent = data.frame(domain =  character(), last_visit = character())
+
+#' A throttled version of GET
+#' 
+#' This uses \code{httr::GET} to fetch a web page, but throttles based on domains.
+#' 
+#' \code{slowGET} keeps a list of domains recently accessed by itself in a 
+#' separate environment.  If a domain has been accessed since \code{pause}
+#' seconds ago, it will delay execution until that time has passed
+#' 
+#' @param pause The time between calls to a domain. Defaults to 10. Can be set
+#' generally with \code{options(SlowGetPause=VALUE)}.
+#' @param url URL to getch
+#' @param ... other arguments to pass to \code{httr::GET}
+#' 
+#' @importFrom stringi stri_replace_first_regex
+#' @importFrom httr GET
+#' @export
+slowGET = function(url, pause=NULL, ...) {
+  
+  if(is.null(pause)) pause = getOption("SlowGetPause")
+  if(is.null(pause)) pause = 10
+  
+  domain = stri_replace_first_regex(url, "https?://[^\\.]*\\.([^/\\s]+)/?[^\\s]*", "$1")
+  if(domain %in% throttle$recent$domain) {
+      next_time = throttle$recent$last_visit[domain == throttle$recent$domain] + 
+                    pause
+      while(Sys.time() < next_time) {
+          dum <- 0
+        }
+      throttle$recent$last_visit[domain == throttle$recent$domain] = Sys.time()
+  } else {
+    throttle$recent = rbind(throttle$recent,
+                      data.frame(domains = domain, last_visit = Sys.time()))
+  }
+
+  throttle$recent = throttle$recent[throttle$recent$last_visit > (Sys.time() - pause),]
+  GET(url, ...)
+}
