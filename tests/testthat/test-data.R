cad1389b081377784d2affe5509e03745b16d953
Noam Ross <noam.ross@gmail.com>
2014-06-27 18:50:49 -0700

    Added test DOIs to each journal in scraper files

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,19 +1,4 @@
 # Tests that data provided is correct
-context("functions")
-
-test_that("get_pub_history works", {
-  testval = structure(list(doi = "10.1098/rspb.2005.3311", journal = "Proceedings of the Royal Society B: Biological Sciences", 
-    volume = "273", editor = NA, submitted = structure(12975, class = "Date"), 
-    revised = NA, decision = NA, accepted = structure(13025, class = "Date"), 
-    preprint = NA, finalversion = NA, online = structure(NA_real_, class = "Date"), 
-    issueonline = NA, issuedate = structure(13186, class = "Date")), .Names = c("doi", 
-"journal", "volume", "editor", "submitted", "revised", "decision", 
-"accepted", "preprint", "finalversion", "online", "issueonline", 
-"issuedate"))
-  testpub = get_pub_history("10.1098/rspb.2005.3311")
-  expect_identical(testval, testpub)
-})
-  
 context("data")
 library(stringi)
 #for these tests, load the csv files from journal_data
@@ -43,6 +28,13 @@ for(filename in names(pubtimes_list)) {
     expect_that(!any(stri_detect_regex(pubtimes_list[[filename]]$editor,
                                        "\\s(?!II)[[:upper:]]{2}\\s")), is_true())
   })
-                                                      
+
+    test_that(paste(filename, "has correctly formatted dates"), {
+    expect_that(!any(stri_detect_regex(pubtimes_list[[filename]]$editor, 
+                                       "['\"`]")), is_true())
+    expect_that(!any(stri_detect_regex(pubtimes_list[[filename]]$editor,
+                                       "\\s(?!II)[[:upper:]]{2}\\s")), is_true())
+  })
+           
   
 }
