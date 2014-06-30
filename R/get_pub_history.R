7e7e160758f7c833f6e2d38c5553666ee1f103f5
Noam Ross <noam.ross@gmail.com>
2014-06-30 12:17:14 -0700

    adding verbosity [skip ci]

--- R/get_pub_history.R
+++ R/get_pub_history.R
@@ -92,6 +92,7 @@ get_pub_history = function(doi, verbose=TRUE, sortdomains=TRUE, filename=NULL) {
 #' @importFrom stringi stri_detect_regex stri_match_all_regex stri_opts_regex
 #' stri_detect_fixed
 scrape = function(pubhistory) {
+  print(paste0(as.charactr(pubhistory$doi), ", ", as.character(pubhistory$journal)))
   scraper_no = which(laply(scrapers, function(z) {
                              pubhistory$journal %in% names(z$journals)}))
   if(length(scraper_no) == 0) {
